823ba19a0db6c3dadd3b7322d412bab93172f64a
Grant Pierce <glpierce174@gmail.com>
2022-04-12 15:17:15 -0700

    Initial deployment

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,23 @@
 Rails.application.routes.draw do
-  resources :pin_groups, only: [:create, :update, :destroy]
-  resources :friendships, only: [:destroy, :update, :create]
-  resources :pins, only: [:show, :update, :create, :destroy]
-  resources :users, only: [:create, :update]
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  post "/login", to: "sessions#create"
-  get "/me", to: "sessions#show"
-  post "/accountCheck", to: "sessions#accountCheck"
-  post "/updateCheck", to: "users#updateCheck"
-  delete "/logout", to: "sessions#destroy"
-  get "/requests", to: "friendships#requests"
-  get "/friends", to: "friendships#friends"
-  get "/my_pins", to: "pins#my_pins"
-  get "/my_groups", to: "pin_groups#my_groups"
-  post "/userSearch", to: "users#search"
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    resources :pin_groups, only: [:create, :update, :destroy]
+    resources :friendships, only: [:destroy, :update, :create]
+    resources :pins, only: [:show, :update, :create, :destroy]
+    resources :users, only: [:create, :update]
+    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+    post "/login", to: "sessions#create"
+    get "/me", to: "sessions#show"
+    post "/accountCheck", to: "sessions#accountCheck"
+    post "/updateCheck", to: "users#updateCheck"
+    delete "/logout", to: "sessions#destroy"
+    get "/requests", to: "friendships#requests"
+    get "/friends", to: "friendships#friends"
+    get "/my_pins", to: "pins#my_pins"
+    get "/my_groups", to: "pin_groups#my_groups"
+    post "/userSearch", to: "users#search"
+    # Defines the root path route ("/")
+    # root "articles#index"
+  end
+
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
